0fb9325bbb71a0cca79f47031130f30120e8bb96
sti <86592209+sti-233@users.noreply.github.com>
2025-01-31 23:48:51 +0800

    Release 0.0.1

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("org.jetbrains.kotlin.android") version "2.1.0"
+    id("org.lsposed.lsplugin.jgit") version "1.1" 
+    id("org.lsposed.lsplugin.resopt") version "1.6" 
+    id("org.lsposed.lsplugin.apksign") version "1.4"
+    id("org.lsposed.lsplugin.apktransform") version "1.2" 
+}
+
+val appVerCode = jgit.repo()?.commitCount("refs/remotes/origin/main") ?: 1
+val appVerName: String by rootProject
+
+apksign {
+    storeFileProperty = "releaseStoreFile"
+    storePasswordProperty = "releaseStorePassword"
+    keyAliasProperty = "releaseKeyAlias"
+    keyPasswordProperty = "releaseKeyPassword"
+}
+
+apktransform {
+    copy {
+        when (it.buildType) {
+            "release" -> file("${it.name}/TrialQualityStandalone_${appVerName}.apk")
+            else -> null
+        }
+    }
+}
+
+android {
+    namespace = "io.github.tqsa"
+    compileSdk = 35
+    buildToolsVersion = "36.0.0-rc4"
+
+    defaultConfig {
+        applicationId = "io.github.tqsa"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = appVerCode
+        versionName = appVerName
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_23
+        targetCompatibility = JavaVersion.VERSION_23
+    }
+
+    kotlinOptions {
+        jvmTarget = "23"
+        freeCompilerArgs = listOf(
+            "-Xno-param-assertions",
+            "-Xno-call-assertions",
+            "-Xno-receiver-assertions",
+            "-language-version=2.0",
+        )
+    }
+}
+
+configurations.all {
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk7")
+    exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
+}
+
+dependencies {
+    compileOnly(files("lib/XposedBridgeApi-89.jar"))
+}
---
+++ app/src/main/java/io/github/tqsa/TrialQuality.kt
@@ -0,0 +1,77 @@
+package io.github.tqsa
+
+import android.util.Log
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class Hook : IXposedHookLoadPackage {
+
+    val targetPackage = listOf("tv.danmaku.bili", "com.bilibili.app.in", "tv.danmaku.bilibilihd", "com.bilibili.app.blue")
+
+    fun hookMethod(className: String, methodName: String, type: Boolean , classLoader: ClassLoader) {
+        XposedHelpers.findAndHookMethod(
+            className,
+            classLoader,
+            methodName,
+            XC_MethodReplacement.returnConstant(type)
+        )
+        Logger("[Success] Hooked $className !")
+    }
+
+    fun hookSetTrue(className: String, classLoader: ClassLoader) {
+        try {
+            XposedHelpers.findAndHookMethod(
+                className,
+                classLoader,
+                "setIsNeedTrial",
+                Boolean::class.javaPrimitiveType,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        Logger("[Hooked] Forcing setIsNeedTrial(true)")
+                        param.args[0] = true
+                    }
+                }
+            )
+            Logger("[Success] Hook applied to $className.setIsNeedTrial")
+        } catch (e: NoSuchMethodError) {
+            Logger("[Error] Method not found: ${e.message}")
+        } catch (e: ClassNotFoundException) {
+            Logger("[Error] Class not found: ${e.message}")
+        } catch (e: Throwable) {
+            Logger("[Fatal] Unexpected error: ${e.stackTraceToString()}")
+        }
+    }
+
+    override fun handleLoadPackage(loadPackageParam: XC_LoadPackage.LoadPackageParam) {
+        if (loadPackageParam.packageName in targetPackage) {
+            XposedBridge.hookAllMethods(
+                XposedHelpers.findClass("android.app.ActivityThread", loadPackageParam.classLoader),
+                "performLaunchActivity",
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        hookSetTrue("com.bapis.bilibili.pgc.gateway.player.v2.SceneControl", loadPackageParam.classLoader)
+                        hookSetTrue("com.bapis.bilibili.playershared.VideoVod", loadPackageParam.classLoader)
+                        hookMethod("com.bapis.bilibili.pgc.gateway.player.v2.SceneControl", "getIsNeedTrial", true , loadPackageParam.classLoader)
+                        hookMethod("com.bapis.bilibili.playershared.VideoVod", "getIsNeedTrial", true , loadPackageParam.classLoader)
+                        hookMethod("com.bapis.bilibili.app.playurl.v1.StreamInfo", "getVipFree", true , loadPackageParam.classLoader)
+                        hookMethod("com.bapis.bilibili.playershared.StreamInfo", "getVipFree", true , loadPackageParam.classLoader)
+                        hookMethod("com.bapis.bilibili.app.playurl.v1.StreamInfo", "getNeedVip", false , loadPackageParam.classLoader)
+                        hookMethod("com.bapis.bilibili.playershared.StreamInfo", "getNeedVip", false , loadPackageParam.classLoader)
+                    }
+                }
+            )
+            Logger("[Success] Hooked ${loadPackageParam.packageName} !")
+        } else {
+            Logger("[Error] Why you want to hook ${loadPackageParam.packageName} ?")
+        }
+    }
+
+    private fun Logger(msg: String) {
+        Log.v("TrialQualityStandalone: ",msg)
+    }
+}
